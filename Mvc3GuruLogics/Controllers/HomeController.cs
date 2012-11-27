949e0b5c00e7ebb6ca9d3b1636ed03778acb17e7
Mirza Salman Niaz <sniaz@asicentral.com>
2012-11-27 18:26:05 +0500

    setup 2

---
+++ Mvc3GuruLogics/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc3GuruLogics.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            
+            ViewBag.CurPage = "Home";
+            return View();
+        }
+
+        public ActionResult AboutUs() 
+        {
+            ViewBag.CurPage = "AboutUs";
+            
+            return View();
+        
+        }
+
+
+        public ActionResult ContactUs()
+        {
+            ViewBag.CurPage = "ContactUs";
+
+            return View();
+
+        }
+
+
+        public ActionResult Services()
+        {
+            ViewBag.CurPage = "Services";
+
+            return View();
+
+        }
+
+    }
+}
